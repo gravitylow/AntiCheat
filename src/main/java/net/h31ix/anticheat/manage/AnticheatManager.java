26bd4e0f78d585dcc5ece5dc3af4b6609691f1df
h31ix <zero_gravity@outlook.com>
2013-10-13 13:46:55 -0400

    Log messages using prefix. Fixes #252

--- src/main/java/net/h31ix/anticheat/manage/AnticheatManager.java
+++ src/main/java/net/h31ix/anticheat/manage/AnticheatManager.java
@@ -84,7 +84,7 @@ public class AnticheatManager {
 
     public void log(String message, int i) {
         if (i != 1 && getConfiguration().getConfig().logToConsole.getValue()) {
-            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + message);
+            plugin.getLogger().info(ChatColor.RED + message);
         }
         if (i == 0 && getConfiguration().getConfig().fileLogLevel.getValue() == LOG_LEVEL_HIGH) // Not an alert, normal log message
         {
